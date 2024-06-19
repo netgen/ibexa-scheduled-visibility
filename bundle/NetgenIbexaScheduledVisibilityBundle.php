3a3489ed1362044dbee5b14f922f7e0fe42c5f46
Petar Jakopec <petar.jakopec0@gmail.com>
2024-06-19 11:42:01 +0200

    NGSTACK-842 add bundle class

---
+++ bundle/NetgenIbexaScheduledVisibilityBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\Bundle\IbexaScheduledVisibilityBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class NetgenIbexaScheduledVisibilityBundle extends Bundle {}
