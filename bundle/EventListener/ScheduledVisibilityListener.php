439ef7ee845f9d085d1a813ba4c057635bae2b77
Petar Jakopec <petar.jakopec0@gmail.com>
2024-06-20 07:26:53 +0200

    NGSTACK-842 add onPublish event listener

---
+++ bundle/EventListener/ScheduledVisibilityListener.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\Bundle\IbexaScheduledVisibilityBundle\EventListener;
+
+use Ibexa\Contracts\Core\Repository\Events\Content\PublishVersionEvent;
+use Netgen\Bundle\IbexaScheduledVisibilityBundle\Service\ScheduledVisibilityService;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+final class ScheduledVisibilityListener implements EventSubscriberInterface
+{
+    public function __construct(
+        private readonly ScheduledVisibilityService $scheduledVisibilityService
+    ) {}
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            PublishVersionEvent::class => ['onPublishVersion'],
+        ];
+    }
+
+    public function onPublishVersion(PublishVersionEvent $event): void
+    {
+        $content = $event->getContent();
+        if ($this->scheduledVisibilityService->accept($content)) {
+            $this->scheduledVisibilityService->toggleVisibility($content);
+        }
+    }
+}
