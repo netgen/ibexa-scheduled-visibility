81691affc6f3156c74666d842d0a97fcb6162bc6
Petar Jakopec <petar.jakopec0@gmail.com>
2024-06-24 14:01:48 +0200

    NGSTACK-842 add handlers registry

---
+++ bundle/DependencyInjection/Compiler/ScheduledVisibilityHandlerRegistrationPass.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\Bundle\IbexaScheduledVisibilityBundle\DependencyInjection\Compiler;
+
+use LogicException;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Reference;
+
+use function sprintf;
+
+final class ScheduledVisibilityHandlerRegistrationPass implements CompilerPassInterface
+{
+    private string $handlerRegistryId = 'Netgen\Bundle\IbexaScheduledVisibilityBundle\ScheduledVisibility\Registry';
+    private string $handlerTag = 'netgen.ibexa_scheduled_visibility.handler';
+
+    public function process(ContainerBuilder $container): void
+    {
+        if (!$container->has($this->handlerRegistryId)) {
+            return;
+        }
+
+        $handlerRegistryDefinition = $container->getDefinition($this->handlerRegistryId);
+
+        $handlers = $container->findTaggedServiceIds($this->handlerTag);
+
+        foreach ($handlers as $id => $attributes) {
+            $this->registerResolver($handlerRegistryDefinition, $id, $attributes);
+        }
+    }
+
+    private function registerResolver(Definition $handlerRegistryDefinition, string $id, array $attributes): void
+    {
+        foreach ($attributes as $attribute) {
+            if (!isset($attribute['identifier'])) {
+                throw new LogicException(
+                    sprintf(
+                        "'%s' service tag needs an 'identifier' attribute to identify the handler",
+                        $this->handlerTag,
+                    ),
+                );
+            }
+
+            $handlerRegistryDefinition->addMethodCall(
+                'register',
+                [
+                    $attribute['identifier'],
+                    new Reference($id),
+                ],
+            );
+        }
+    }
+}
---
+++ bundle/ScheduledVisibility/Registry.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\Bundle\IbexaScheduledVisibilityBundle\ScheduledVisibility;
+
+use OutOfBoundsException;
+
+use function sprintf;
+
+final class Registry
+{
+    /**
+     * @var ScheduledVisibilityInterface[]
+     */
+    private array $handlerMap = [];
+
+    /**
+     * @param ScheduledVisibilityInterface[] $handlerMap
+     */
+    public function __construct(array $handlerMap = [])
+    {
+        foreach ($handlerMap as $identifier => $handler) {
+            $this->register($identifier, $handler);
+        }
+    }
+
+    public function register(string $identifier, ScheduledVisibilityInterface $handler): void
+    {
+        $this->handlerMap[$identifier] = $handler;
+    }
+
+    public function get(string $identifier): ScheduledVisibilityInterface
+    {
+        return $this->handlerMap[$identifier] ?? throw new OutOfBoundsException(
+            sprintf(
+                "No handler is registered for identifier '%s'",
+                $identifier,
+            ),
+        );
+    }
+}
