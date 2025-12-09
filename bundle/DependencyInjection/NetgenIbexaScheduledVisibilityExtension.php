62f401550c563db69cbe93cdd2b404a206823220
Edi Modrić <edi.modric@gmail.com>
2025-12-09 13:23:45 +0100

    Use non-deprecated Extension class

--- bundle/DependencyInjection/NetgenIbexaScheduledVisibilityExtension.php
+++ bundle/DependencyInjection/NetgenIbexaScheduledVisibilityExtension.php
@@ -6,8 +6,8 @@ namespace Netgen\Bundle\IbexaScheduledVisibilityBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 final class NetgenIbexaScheduledVisibilityExtension extends Extension
 {
